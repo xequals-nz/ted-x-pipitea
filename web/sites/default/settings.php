876416715b038f961908d4aff794e5fd4896768d
Rox Flame <rox@roxflame.com>
2024-05-01 15:39:33 +1000

    Reapply pantheon stuff, update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -14,8 +14,41 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  *      a local development environment, to ensure that
  *      the site settings remain consistent.
  */
-include __DIR__ . "/settings.pantheon.php";
+ 
+ 
+ /**
+ * Set the default location for the 'private' directory.  Note
+ * that this location is protected when running on the Pantheon
+ * environment, but may be exposed if you migrate your site to
+ * another environment.
+ */
+$settings['file_private_path'] = 'sites/default/files/private';
 
+// Check to see if we are serving an installer page.
+$is_installer_url = (strpos($_SERVER['SCRIPT_NAME'], '/core/install.php') === 0);
+
+/**
+ * Add the Drupal 8 CMI Directory Information directly in settings.php to make sure
+ * Drupal knows all about that.
+ *
+ * Issue: https://github.com/pantheon-systems/drops-8/issues/2
+ *
+ * IMPORTANT SECURITY NOTE:  The configuration paths set up
+ * below are secure when running your site on Pantheon.  If you
+ * migrate your site to another environment on the public internet,
+ * you should relocate these locations. See "After Installation"
+ * at https://www.drupal.org/node/2431247
+ *
+ */
+if ($is_installer_url) {
+  $settings['config_sync_directory'] = 'sites/default/files';
+}
+else {
+  $settings['config_sync_directory'] = getenv('DOCROOT') ? '../config' : 'sites/default/config';
+}
+
+
+ 
 /**
  * Skipping permissions hardening will make scaffolding
  * work better, but will also raise a warning when you
@@ -32,3 +65,45 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+/**
+ * Database settings:
+ *
+ * The $databases array specifies the database connection or
+ * connections that Drupal may use.  Drupal is able to connect
+ * to multiple databases, including multiple types of databases,
+ * during the same request.
+ *
+ * One example of the simplest connection array is shown below. To use the
+ * sample settings, copy and uncomment the code below between the @code and
+ * @endcode lines and paste it after the $databases declaration. You will need
+ * to replace the database username and password and possibly the host and port
+ * with the appropriate credentials for your database system.
+ *
+ * The next section describes how to customize the $databases array for more
+ * specific needs.
+ *
+ * @code
+ * $databases['default']['default'] = [
+ *   'database' => 'databasename',
+ *   'username' => 'sqlusername',
+ *   'password' => 'sqlpassword',
+ *   'host' => 'localhost',
+ *   'port' => '3306',
+ *   'driver' => 'mysql',
+ *   'prefix' => '',
+ *   'collation' => 'utf8mb4_general_ci',
+ * ];
+ * @endcode
+ */
+ * $databases['default']['default'] = [
+ *   'database' => 'u793468219_xequals',
+ *   'username' => 'u793468219_xequals',
+ *   'password' => 'Pe@ceLily88',
+ *   'host' => 'localhost',
+ *   'port' => '3306',
+ *   'driver' => 'mysql',
+ *   'prefix' => '',
+ *   'collation' => 'utf8mb4_general_ci',
+ * ];
+
